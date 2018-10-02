38c538df9ebadb6ecb4d84e2945337a73e8079ec
yanBinChen <1247585704@qq.com>
2018-10-02 11:02:03 +0800

    Update and rename 6.二叉树的前序遍历(递归和非递归版本).java to 6.二叉树的先序遍历(递归和非递归版本).java

---
+++ 6.二叉树的先序遍历(递归和非递归版本).java
@@ -0,0 +1,37 @@
+难易程度：★★
+ 重要性：★★★★★
+ 树结构是面试中的考察的重点，而树的遍历又是树结构的基础。
+ 
+//先序遍历，递归版本
+public static ArrayList<Integer> preOrder(TreeNode root) {
+        ArrayList<Integer> res = new ArrayList<Integer>();
+         Stack<TreeNode> s = new Stack<TreeNode>();
+        s.push(root);
+        while (!s.isEmpty()) {
+            TreeNode tem = s.pop();// 根节点出栈
+             // 把根节点加入结果集 前序遍历顺序：根、左，右 所以对应的进栈顺序为：右、左（根节点进栈后马上出栈了）
+            res.add(tem.val);
+            if (tem.right != null)
+                s.push(tem.right);
+            if (tem.left != null)
+                s.push(tem.left);
+        }
+        return res;
+    }
+    
+    /**
+     * 先序遍历，递归版本
+     * @param root
+     * @return
+     */
+    public static ArrayList<Integer> preOrder(TreeNode root){
+        ArrayList<Integer> res = new ArrayList<Integer>();
+         preOrder(root,res);
+        return res;
+    }
+    private static void preOrder(TreeNode root,ArrayList<Integer> res) {
+        if(root==null)  return;
+         res.add(root.val);
+        preOrder(root.left);
+        preOrder(root.right);
+    }
